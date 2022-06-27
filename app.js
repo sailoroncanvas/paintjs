8d05e84f26428bdf1f39b17be1737d41a4866013
PARK JI EUN <pjieuniv20@ewhain.net>
2022-06-28 07:20:51 +0900

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
 
-const INIRIAL_COLOR = "#2c2c2c";
+const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
@@ -13,8 +13,8 @@ canvas.height = CANVAS_SIZE;
 
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-ctx.strokeStyle = INIRIAL_COLOR;
-ctx.fillStyle = INIRIAL_COLOR;
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -75,25 +75,21 @@ function handleSaveClick() {
   const image = canvas.toDataURL();
   const link = document.createElement("a");
   link.href = image;
-  link.download = "PaintJS[🎨]";
+  link.download = "PaintJS";
   link.click();
 }
 
 if (canvas) {
-  canvas.addEventListener("mousemove", onMouseMove, false);
-  canvas.addEventListener("mousedown", startPainting, false);
-  canvas.addEventListener("mouseup", stopPainting, false);
-  canvas.addEventListener("mouseleave", stopPainting, false);
-  canvas.addEventListener("click", handleCanvasClick, false);
-  strokeInput.addEventListener("input", onRangeChange, false);
-  fillBtn.addEventListener("click", startFilling, false);
-  drawBt.addEventListener("click", stopFilling, false);
-  saveBtn.addEventListener("click", stopFilling, false);
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
   canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach((color) =>
-  color.addEventListener("click", onColorClick, false)
+  color.addEventListener("click", handleColorClick)
 );
 
 if (range) {
@@ -107,4 +103,3 @@ if (mode) {
 if (saveBtn) {
   saveBtn.addEventListener("click", handleSaveClick);
 }
-
